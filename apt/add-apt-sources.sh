b2d3f03b4c279fe18f632f31458cfd0bac7dec37
Jake Howard <git@theorangeone.net>
2016-02-12 10:11:16 +0000

    Only add source if doesnt already exist

--- apt/add-apt-sources.sh
+++ apt/add-apt-sources.sh
@@ -1,6 +1,21 @@
 #!/usr/bin/env bash
 
-echo "deb http://downloads.hipchat.com/linux/apt stable main" > /etc/apt/sources.list.d/atlassian-hipchat.list
-echo "deb http://apt.insynchq.com/ubuntu wily non-free contrib" > /etc/apt/sources.list.d/insync.list
-echo "deb http://download.virtualbox.org/virtualbox/debian wily contrib" > /etc/apt/sources.list.d/virtualbox.list
-echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_15.10/ /' >> /etc/apt/sources.list.d/owncloud-client.list
+
+if [ -a /etc/apt/sources.list.d/atlassian-hipchat.list ]; then
+  echo "deb http://downloads.hipchat.com/linux/apt stable main" >> /etc/apt/sources.list.d/atlassian-hipchat.list
+fi
+
+
+if [ -a /etc/apt/sources.list.d/insync.list ]; then
+  echo "deb http://apt.insynchq.com/ubuntu wily non-free contrib" >> /etc/apt/sources.list.d/insync.list
+fi
+
+
+if [ -a /etc/apt/sources.list.d/virtualbox.list ]; then
+  echo "deb http://download.virtualbox.org/virtualbox/debian wily contrib" >> /etc/apt/sources.list.d/virtualbox.list
+if
+
+
+if [ -a /etc/apt/sources.list.d/owncloud-client.list ]; then
+  echo 'deb http://download.opensuse.org/repositories/isv:/ownCloud:/desktop/xUbuntu_15.10/ /' >> /etc/apt/sources.list.d/owncloud-client.list
+fi
