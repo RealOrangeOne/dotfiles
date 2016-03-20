3c1db96f15e2528410bbc5b037884b110dae4a38
RealOrangeOne <git@theorangeone.net>
2016-03-20 19:38:55 +0000

    Actually run commands

--- yaourt/export.py
+++ yaourt/export.py
@@ -4,4 +4,4 @@ import os, json
 os.system('sudo yaourt -Syau')
 
 packages = " ".join(json.load(open('packages.json')))
-print("sudo yaourt -S {} ".format(packages))
+os.system("sudo yaourt -S {} ".format(packages))
