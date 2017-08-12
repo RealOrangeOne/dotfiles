dc527c216b0e28d486c3231b2011f773676d0109
Jake Howard <git@theorangeone.net>
2017-08-12 18:43:29 +0100

    Add global gitignore

--- modules/config/manifests/init.pp
+++ modules/config/manifests/init.pp
@@ -18,4 +18,16 @@ class config {
     mode => "0644",
     source => 'puppet:///modules/config/uniemoji.json'
   }
+
+  file { '/home/jake/.gitignore_global':
+    ensure => file,
+    mode => "0644",
+    source => 'puppet:///modules/config/gitignore_global'
+  }
+
+  git::config { "global ignore" :
+    key => 'core.excludesfile',
+    value => '/home/jake/.gitignore_global',
+    user => "jake"
+  }
 }
