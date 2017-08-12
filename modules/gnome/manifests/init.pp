d724d34b62bc4e1078cccb96963917ff9e011eb4
Jake Howard <git@theorangeone.net>
2017-08-12 14:36:10 +0100

    Add config for nautilus tempalates

---
+++ modules/gnome/manifests/init.pp
@@ -0,0 +1,10 @@
+class gnome {
+  file { '/home/jake/Templates':
+    ensure => directory,
+    source => 'puppet:///modules/gnome/Templates',
+    owner => "jake",
+    group => "users",
+    mode => "0755",
+    recurse => remote
+  }
+}
