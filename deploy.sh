161fa05240f554251bf4e4c7fc5526818b596037
Jake Howard <git@theorangeone.net>
2017-10-11 21:29:53 +0100

    Dont break if patch applied

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-patch /usr/bin/makepkg < makepkg.patch
+patch /usr/bin/makepkg -N < makepkg.patch
 
 puppet apply manifests/main.pp --modulepath=modules/ --verbose
 
-patch /usr/bin/makepkg -R < makepkg.patch
+patch /usr/bin/makepkg -NR < makepkg.patch
