f1358eaebb82cc5381e2487124e85a8c90e4dd56
Jake Howard <git@theorangeone.net>
2017-10-19 14:16:25 +0100

    Remove youtubedl repo

--- modules/yaourt/manifests/keys.pp
+++ modules/yaourt/manifests/keys.pp
@@ -2,7 +2,6 @@ class yaourt::keys () {
   $keys = [
     '8A8F901A',  # Sublime Text
     'D1483FA6C3C07136',  # Tor Browser
-    'C6DFFE6ED7166B97',  # Youtube-DL
     '9D5F1C051D146843CDA4858BDE64825E7CBC0D51',  # ArchStrike
   ]
 
