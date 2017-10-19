c2a87e3d4bb4f0bff3eb80f730ec07b063d3a1b1
Jake Howard <git@theorangeone.net>
2017-10-19 13:16:10 +0100

    Remove buggy gitconfig command

--- modules/config/manifests/git.pp
+++ modules/config/manifests/git.pp
@@ -6,10 +6,4 @@ class config::git {
     mode => '0644',
     source => 'puppet:///modules/config/gitignore_global'
   }
-
-  -> exec { "install global git config":
-    command => "git config --global core.excludesfile '$global_gitignore'",
-    user => 'jake',
-    unless => 'git config --global core.excludesfile'
-  }
 }
