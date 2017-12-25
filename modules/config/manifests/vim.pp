959fe9b6a56900e3076e9600859a50256ae467dd
Jake Howard <git@theorangeone.net>
2017-12-25 22:25:17 +0000

    Install custom vimrc for root user too

--- modules/config/manifests/vim.pp
+++ modules/config/manifests/vim.pp
@@ -8,11 +8,20 @@ class config::vim {
     source => 'puppet:///modules/config/vimrc'
   }
 
+  -> file { 'vimrc for root':
+    ensure => file,
+    mode   => '0644',
+    owner  => 'root',
+    group  => 'users',
+    path => '/root/.vimrc',
+    source => 'puppet:///modules/config/vimrc'
+  }
+
   -> vcsrepo { 'Vim runtime':
     ensure => latest,
     provider => git,
-    user => 'jake',
-    path => '/home/jake/.vim_runtime',
+    user => 'root',
+    path => '/usr/share/amix_vimrc',
     source => 'https://github.com/amix/vimrc.git',
   }
 }
