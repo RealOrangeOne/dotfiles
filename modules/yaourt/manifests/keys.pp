487f23e413d2e702a5d13439cb6adb9c5adfa877
Jake <git@theorangeone.net>
2018-03-14 12:03:39 +0000

    Add AUR cache repo so i don't have to compile everything

--- modules/yaourt/manifests/keys.pp
+++ modules/yaourt/manifests/keys.pp
@@ -3,6 +3,7 @@ class yaourt::keys () {
     '8A8F901A',  # Sublime Text
     'D1483FA6C3C07136',  # Tor Browser
     '9D5F1C051D146843CDA4858BDE64825E7CBC0D51',  # ArchStrike
+    '7448C890582975CD'
   ]
 
   $keys.each |$key| {
