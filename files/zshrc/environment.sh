a32e5810dd2112b7dcb676ae1961231ccfe795fa
Jake Howard <git@theorangeone.net>
2018-10-25 18:55:16 +0100

    Remove home bin directory

--- files/zshrc/environment.sh
+++ files/zshrc/environment.sh
@@ -3,7 +3,6 @@
 export PATH=${PATH}:${ANDROID_HOME}/tools
 export PATH=${PATH}:${ANDROID_HOME}/platform-tools
 export PATH=${PATH}:/opt/genymobile/genymotion
-export PATH=${PATH}:${HOME}/.bin
 
 export DOTFILES=${HOME}/.dotfiles
 
