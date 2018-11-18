448e02e5640bed9016976875a56528c101f451d0
Jake Howard <git@theorangeone.net>
2018-11-18 20:26:59 +0000

    Replace i3status-rust with polybar

---
+++ files/launch-polybar.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# From: https://github.com/jaagr/polybar/issues/763#issuecomment-392960721
+
+pkill polybar
+
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload example &
+done
