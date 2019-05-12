6162219ebfa67fa280260fd0ee1f6b6b355d440b
Jake Howard <git@theorangeone.net>
2019-05-12 21:24:32 +0100

    Remove unnecessary comment

--- files/zshrc/base.sh
+++ files/zshrc/base.sh
@@ -20,5 +20,3 @@ alias grep='grep --color=auto'
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
 [ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
-
-# don't put duplicate lines or lines starting with space in the history.
