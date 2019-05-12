d2927daeb8b41566bd3e1b90118556498564954b
Jake Howard <git@theorangeone.net>
2019-05-12 10:57:06 +0100

    Remove unused android SDK variables

--- files/zshrc/environment.sh
+++ files/zshrc/environment.sh
@@ -1,9 +1,3 @@
-
-# Extend path
-export PATH=${PATH}:${ANDROID_HOME}/tools
-export PATH=${PATH}:${ANDROID_HOME}/platform-tools
-export PATH=${PATH}:/opt/genymobile/genymotion
-
 export DOTFILES=${HOME}/.dotfiles
 
 export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
