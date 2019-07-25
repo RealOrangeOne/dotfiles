6c6fc68f4a15e42314735e2d4b4f88eece9b5a85
Jake Howard <git@theorangeone.net>
2019-07-25 08:39:08 +0100

    Add crab configuration variables

--- files/zshrc/catfish.sh
+++ files/zshrc/catfish.sh
@@ -26,3 +26,8 @@ alias ctfs="cd ~/catfish && ./start && cd - > /dev/null"
 alias ctpt="NO_MIGRATIONS=true IN_TEST=true FILE_STORAGE='django.core.files.storage.FileSystemStorage' ctpr manage.py test $@ --keepdb"
 
 export CATFISH_HOSTNAME_OVERRIDE=localhost
+
+# Crab
+export ENV_FILE=etc/environments/development/env,.env
+export PROC_FILE=etc/environments/development/procfile
+export BIN_DIRS=env/bin
