033a5e203fcb029c274fcdcd42f60862e05d9f40
Jake Howard <git@theorangeone.net>
2019-09-03 12:35:31 +0100

    No need to have polybar listen for FS events

--- files/launch-polybar.sh
+++ files/launch-polybar.sh
@@ -5,6 +5,6 @@
 pkill polybar
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload top &
-  MONITOR=$m polybar --reload bottom &
+  MONITOR=$m polybar top &
+  MONITOR=$m polybar bottom &
 done
