a0c2da6d70c998511a1c480476d88487a01d0e96
Jake Howard <git@theorangeone.net>
2021-12-20 19:57:39 +0000

    Inline zshrc
    It really doesn't need to be multiple files. And it's easier to manage this way

--- files/zshrc/crab.sh
+++
@@ -1,5 +0,0 @@
-alias ctpt="NO_MIGRATIONS=true IN_TEST=true FILE_STORAGE='django.core.files.storage.FileSystemStorage' crab manage.py test $@ --keepdb"
-
-export ENV_FILE=etc/environments/development/env,.env
-export PROC_FILE=etc/environments/development/procfile
-export BIN_DIRS=env/bin
--- files/zshrc/environment.sh
+++
@@ -1,7 +0,0 @@
-export DOTFILES=${HOME}/.dotfiles
-
-export TIMEFMT=$'\nreal\t%E\nuser\t%U\nsys\t%S'
-export HISTCONTROL=ignoreboth
-
-export BROWSER=$(which firefox)
-export EDITOR=$(which nvim)
