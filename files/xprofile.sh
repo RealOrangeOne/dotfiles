fbf2b4598fd04dbac8b2d48cc2269ac088818a1b
Jake Howard <git@theorangeone.net>
2023-07-31 14:25:31 +0100

    Add xprofile to start SSH agent

---
+++ files/xprofile.sh
@@ -0,0 +1,11 @@
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+export LANG=en_GB.UTF-8
+export ARCHFLAGS="-arch x86_64"
+export MOZ_USE_XINPUT2=1
+
+if ! pgrep -u "$USER" ssh-agent > /dev/null; then
+    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
+fi
+if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
+    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
+fi
