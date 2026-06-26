2a20134cb5bfe72d8383887362944421bfaa0def
Jake Howard <git@theorangeone.net>
2026-06-26 15:24:28 +0100

    Replace manual ssh-agent management with systemd
    Also removes xprofile in favour of plasma native mechanism

--- files/env.sh
+++ files/env.sh
@@ -2,12 +2,5 @@ export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quo
 export LANG=en_GB.UTF-8
 export ARCHFLAGS="-arch x86_64"
 export MOZ_USE_XINPUT2=1
-
 export SSH_ASKPASS=ksshaskpass
-
-if ! pgrep -u "$USER" ssh-agent > /dev/null; then
-    ssh-agent > "$XDG_RUNTIME_DIR/ssh-agent.env"
-fi
-if [[ ! -f "$SSH_AUTH_SOCK" ]]; then
-    source "$XDG_RUNTIME_DIR/ssh-agent.env" >/dev/null
-fi
+export DOTFILES=${HOME}/.dotfiles
