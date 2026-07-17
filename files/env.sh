82d1f10961f885a5bb5276f6aa98f413a7643660
Jake Howard <git@theorangeone.net>
2026-07-17 10:01:53 +0100

    Set SSH auth socket path

--- files/env.sh
+++ files/env.sh
@@ -3,4 +3,5 @@ export LANG=en_GB.UTF-8
 export ARCHFLAGS="-arch x86_64"
 export MOZ_USE_XINPUT2=1
 export SSH_ASKPASS=ksshaskpass
+export SSH_AUTH_SOCK=/run/user/1000/ssh-agent.socket
 export DOTFILES=${HOME}/.dotfiles
